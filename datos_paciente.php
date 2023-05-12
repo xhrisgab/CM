61751ccaf6774316a7fd13651dd2fd14ea88564e
xhrisgab <xhris.gab@gmail.com>
2023-05-11 22:58:39 -0400

    corregido insert y

--- datos_paciente.php
+++ datos_paciente.php
@@ -2,6 +2,7 @@
 include('./conexion/conexion.php');
 $mysqli = conectar();
 
+$codigo = $_POST['codigo'];
 $nombre = $_POST['firstName'];
 $lastName1 = $_POST['lastName1'];
 $lastName2 = $_POST['lastName2'];
@@ -12,15 +13,48 @@ $sex = $_POST['sex'];
 
 if (mysqli_connect_errno()) {
     return 'No conectado';
-}else{
-    echo 'Conectado';}
+}
+/*else{
+    echo 'Conectado';}*/
 
-$sql = "INSERT INTO PACIENTE VALUES ('".$nombre."','".$lastName1."','".$lastName2."','".$birthdat."','".$address."','".$celular."','".$sex."')";
-echo $sql;
+$sql = "INSERT INTO PACIENTE VALUES (".$codigo.",'".$nombre."','".$lastName1."','".$lastName2."','".$birthdat."','".$address."','".$celular."','".$sex."')";
 //    echo $sql;
         if (mysqli_query($mysqli, $sql) === TRUE) {
-            printf("Se creó con éxtio la tabla myCity.\n");
-        } else {
+    //         echo '<script type="text/javascript">
+    // alert("Se creo el Paciente de forma correcta");
+    // window.location.href="registro_paciente.php";
+    // </script>';
+    // exit(0);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paciente Registrado</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <label for="Registro">Paciente Registrado correctamente...</label>
+                <form action="registro_paciente.php" method="post">
+
+                    <button type="submit" class="btn btn-secondary">Atras...</button>
+                </form>
+            </div>   
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+</html>
+
+<?php
+} else {
             echo "Error: " . $sql . "" . mysqli_error($conn);
         }
 ?>
--- registro_paciente.php
+++ registro_paciente.php
@@ -14,37 +14,59 @@
 <body>
     <div class="container">
         <h2>Datos del paciente</h2>
-        <form action="datos_paciente.php" method="post">
-            <div class="form-group">
-                <label for="Nombre">Nombre del Paciente:</label>
-                <input type="text" class="form-control" id="Nombre" placeholder="Ingresa el nombre del paciente" name="firstName">
+        <div class="row justify-content-md-center">
+            <div class="col-lg-8 col-md-auto">
+
+                <form action="datos_paciente.php" method="post">
+                    <div class="form-group">
+                        <label for="CI">Codigo del paciente:</label>
+                        <input type="number" class="form-control" id="codigo" placeholder="Ingresa el codigo del paciente" name="codigo" min="1" max="999999999999">
+                    </div>
+                    <div class="form-group">
+                        <label for="Nombre">Nombre del Paciente:</label>
+                        <input type="text" class="form-control" id="Nombre" placeholder="Ingresa el nombre del paciente" name="firstName">
+                    </div>
+                    <div class="form-group">
+                        <label for="Apellido1">Apellido del Paciente1:</label>
+                        <input type="text" class="form-control" id="Apellido1" placeholder="Apellido Paterno" name="lastName1">
+                    </div>
+                    <div class="form-group">
+                        <label for="Apellid2">Apellido del Paciente2:</label>
+                        <input type="text" class="form-control" id="Apellido2" placeholder="Apellido Materno" name="lastName2">
+                    </div>
+                    <div class="form-group">
+                        <label for="Data">Fecha de Nacimiento:</label>
+                        <input type="date" class="form-control" id="fechaNac" placeholder="Fecha de nacimiento" name="birthday">
+                    </div>
+                    <div class="form-group">
+                        <label for="Direccion">Direccion del paciente:</label>
+                        <input type="text" class="form-control" id="address" placeholder="Direccion del paciente" name="address">
+                    </div>
+                    <div class="form-group">
+                        <label for="Cellphone">Celphone:</label>
+                        <input type="number" class="form-control" id="celular" placeholder="cellphone" name="celular">
+                    </div>
+                    <div class="form-group">
+                        <label for="Sexo">Genero:</label>
+                        <select class="form-select" aria-label="Default select example" name="sex" id="sex">
+                            <option selected>Seleccione una opcion...</option>
+                            <option value="M">Masculino</option>
+                            <option value="F">Femenino</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-success">Registrar</button>
+                </form>
             </div>
-            <div class="form-group">
-                <label for="Apellido1">Apellido del Paciente1:</label>
-                <input type="text" class="form-control" id="Apellido1" placeholder="Apellido Paterno" name="lastName1">
-            </div>
-            <div class="form-group">
-                <label for="Apellid2">Apellido del Paciente2:</label>
-                <input type="text" class="form-control" id="Apellido2" placeholder="Apellido Materno" name="lastName2">
-            </div>
-            <div class="form-group">
-                <label for="Data">Fecha de Nacimiento:</label>
-                <input type="date" class="form-control" id="fechaNac" placeholder="Fecha de nacimiento" name="birthday">
-            </div>
-            <div class="form-group">
-                <label for="Direccion">Direccion del paciente:</label>
-                <input type="text" class="form-control" id="fechaNac" placeholder="Fecha de nacimiento" name="address">
-            </div>
-            <div class="form-group">
-                <label for="Cellphone">Celphone:</label>
-                <input type="number" class="form-control" id="cellphone" placeholder="cellphone" name="celular">
-            </div>
-            <div class="form-group">
-                <label for="Sexo">Sexo:</label>
-                <input type="Text" class="form-control" id="cellphone" placeholder="cellphone" name="sex">
-            </div>
-            <button type="submit" class="btn btn-primary">Registrar</button>
-        </form>
+        </div>
+<br>
+        <div class="row">
+            <form action="pag_principal.php" method="post">
+                <input type="hidden" value="admin" name="nombre" id="nombre">
+                <input type="hidden" value="admin" name="pass" id="pass">
+                <button type="submit" class="btn btn-secondary">Atras...</button>
+            </form>
+        </div>
+        
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
