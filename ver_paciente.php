787abf8ac80fa52c902d4fd34c15e46470415287
xhrisgab <xhris.gab@gmail.com>
2023-05-18 10:44:13 -0400

    corregido control de boton para edicion en ver_paciente.php

--- ver_paciente.php
+++ ver_paciente.php
@@ -27,8 +27,6 @@ $sql = "SELECT * FROM paciente WHERE CI='".$ci."'";
     <title>Editar Paciente</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     
 </head>
 
@@ -46,31 +44,31 @@ $sql = "SELECT * FROM paciente WHERE CI='".$ci."'";
                 <form action="datos_paciente.php" method="post" id="control">
                     <div class="form-group">
                         <label for="CI">Carnet de Identidad:</label>
-                        <input type="text" class="form-control" id="ci" name="ci" value="<?php echo $ci;?>">
+                        <input type="text" class="form-control" id="ci" name="ci" value="<?php echo $ci;?>" disabled>
                     </div>
                     <div class="form-group">
                         <label for="Nombre">Nombre del Paciente:</label>
-                        <input type="text" class="form-control" id="nombre" value="<?php echo $row['nombre'];?>" name="nombre">
+                        <input type="text" class="form-control" id="nombre" value="<?php echo $row['nombre'];?>" name="nombre" disabled>
                     </div>
                     <div class="form-group">
                         <label for="Apellido1">Apellido del Paciente1:</label>
-                        <input type="text" class="form-control" id="Apellido1" placeholder="Apellido Paterno" name="lastName1">
+                        <input type="text" class="form-control" id="Apellido1" name="lastName1" disabled>
                     </div>
                     <div class="form-group">
                         <label for="Apellid2">Apellido del Paciente2:</label>
-                        <input type="text" class="form-control" id="Apellido2" placeholder="Apellido Materno" name="lastName2">
+                        <input type="text" class="form-control" id="Apellido2" name="lastName2" disabled>
                     </div>
                     <div class="form-group">
                         <label for="Data">Fecha de Nacimiento:</label>
-                        <input type="date" class="form-control" id="birthday" placeholder="Fecha de nacimiento" name="birthday">
+                        <input type="date" class="form-control" id="birthday" name="birthday"disabled>
                     </div>
                     <div class="form-group">
                         <label for="Direccion">Direccion del paciente:</label>
-                        <input type="text" class="form-control" id="address" placeholder="Direccion del paciente" name="address">
+                        <input type="text" class="form-control" id="address" name="address" disabled>
                     </div>
                     <div class="form-group">
                         <label for="Direccion">Ciudad:</label>
-                        <select class="form-select" aria-label="Default" name="ciudad" id="ciudad">
+                        <select class="form-select" aria-label="Default" name="ciudad" id="ciudad" disabled>
                             <option selected>Seleccione una opcion...</option>
                             <option value="La Paz">La Paz</option>
                             <option value="Oruro">Oruro</option>
@@ -86,20 +84,21 @@ $sql = "SELECT * FROM paciente WHERE CI='".$ci."'";
                     </div>
                     <div class="form-group">
                         <label for="Cellphone">Telefono:</label>
-                        <input type="number" class="form-control" id="telefono" placeholder="telefono" name="telefono">
+                        <input type="number" class="form-control" id="telefono" name="telefono" disabled>
                     </div>
                     <div class="form-group">
                         <label for="Cellphone">Celular:</label>
-                        <input type="number" class="form-control" id="celular" placeholder="celular" name="celular">
+                        <input type="number" class="form-control" id="celular" name="celular" disabled>
                     </div>
                     <div class="form-group">
                         <label for="Sexo">Genero:</label>
-                        <select class="form-select" aria-label="Default select example" name="sex" id="sex">
+                        <select class="form-select" aria-label="Default select example" name="sex" id="sex" disabled>
                             <option selected>Seleccione una opcion...</option>
                             <option value="M">Masculino</option>
                             <option value="F">Femenino</option>
                         </select>
                     </div>
+                    <br>
                     <button type="submit" class="btn btn-success">Registrar</button>
                 </form>
             </div>
@@ -114,25 +113,11 @@ $sql = "SELECT * FROM paciente WHERE CI='".$ci."'";
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-<script>
-    $(document).ready(function(){
-        $("#control :input").prop("disabled", true);
-    });
-    $(".checkbox").click(function() {
-                if (this.value=="Enable") {
-                    this.value = "Disable";
-                    $("#control :input").prop("disabled", false);
-                }
-                else {
-                    this.value = "Enable";
-                    $("#control :input").prop("disabled", true);
-                }
-            });
-    </script>
 
-<script>/*
+<script>
     // Accedemos al botón
     var emailInput = document.getElementById('ci');
+    var formulario = document.forms['control'].elements;
     var estado = document.getElementById('flexSwitchCheckReverse').value;
     var x = false;
 
@@ -140,14 +125,19 @@ $sql = "SELECT * FROM paciente WHERE CI='".$ci."'";
 
     if (x){
         x= false;
-        emailInput.disabled = true;
-        console.log(x);
-
+        for(var i=0;i<formulario.length;i++){
+            //console.log(formulario[i]);
+            formulario[i].disabled = true;
+            //console.log(i);
+        }
     }else{
         x= true;
-        emailInput.disabled = false;
-        console.log(x);
+        for(var i=0;i<formulario.length;i++){
+            //console.log(formulario[i]);
+            formulario[i].disabled = false;
+            //console.log(i);
+        }
     }
-    });*/
+    });
 </script>
 </html>
